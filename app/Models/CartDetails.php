332bb9af34d89e4c34377a9871a2b62d53d8b043
Mahmut Sami <samimahmut34@gmail.com>
2022-05-19 11:45:28 +0300

    'Card & Card detail model and migration is created'

---
+++ app/Models/CartDetails.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CartDetails extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+       'card_detail_id',
+       'card_id',
+       'product_id',
+       'quantity'
+    ];
+}
