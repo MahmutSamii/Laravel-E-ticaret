04de87318748877af13686f9d441dcd930d4039c
Mahmut Sami <samimahmut34@gmail.com>
2022-05-19 11:57:56 +0300

    'Order & Order detail model and migration is created'

--- app/Models/OrderDetails.php
+++ app/Models/OrderDetails.php
@@ -5,16 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CartDetails extends Model
+class OrderDetails extends Model
 {
     use HasFactory;
 
-    protected $primaryKey = 'card_detail_id';
+    protected $primaryKey = 'order_details_id';
 
     protected $fillable = [
-       'card_detail_id',
-       'card_id',
-       'product_id',
-       'quantity'
+        'order_details_id',
+        'order_id',
+        'product_id',
+        'quantity'
     ];
 }
